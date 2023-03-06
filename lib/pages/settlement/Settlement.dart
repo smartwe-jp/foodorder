b1b1853696c1614f08b7df9b794e3ebc455b8c23
lky <liukeyan915@163.com>
2023-03-06 10:42:42 +0900

    删除注释

--- lib/pages/settlement/Settlement.dart
+++ lib/pages/settlement/Settlement.dart
@@ -476,7 +476,7 @@ class _SettlementPageState extends State<SettlementPage> {
       };
       request('webBootToPayv2', method: 'POST', parameters: formData).then((val) {
         var response = json.decode(val.toString());
-        LogUtil.d(response);
+        //LogUtil.d(response);
         if (response['code'] == 200 && response['data'].isNotEmpty) {
           var resultData = response['data'];
           if(resultData["requestInfo"] != ""){
