15ba0e7c4a6da4acd910ccfefefdf9deb80aa7f5
lky <liukeyan915@163.com>
2023-09-07 16:43:06 +0900

    精算扫码页面调整

--- lib/app/modules/SelfCheckoutscanningcode/views/self_checkoutscanningcode_view.dart
+++ lib/app/modules/SelfCheckoutscanningcode/views/self_checkoutscanningcode_view.dart
@@ -268,7 +268,197 @@ class SelfCheckoutscanningcodeView
             AnimatedSwitcher(
               duration: Duration(milliseconds: 500), // 动画持续时间
               child: controller.showCartTotalGoodsNum.value == 0
-                  ? Image.asset('assets/images/public/error_public.jpg', key: UniqueKey()) // 没有数据时显示图片
+                  ? Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: Swiper(
+                      //itemHeight: 200,
+                      itemBuilder: (BuildContext context,int index){
+                        // 配置图片地址
+                        return publicShowMenuImage(imgPath:controller.homeList.value[index],imgWidth: 1080.0,imgHeight: 1920.0);
+                      },
+                      // 配置图片数量
+                      itemCount: controller.homeList.value.length,
+                      // 底部分页器
+                      //pagination: new SwiperPagination(margin: EdgeInsets.only(bottom: ScreenAdapter.height(55))),
+                      // 左右箭头
+                      //control: new SwiperControl(),
+                      // 无限循环
+                      loop: (controller.homeList.value.length >1) ?true :false,
+                      duration: 1000,
+                      autoplayDelay:12000,
+                      // 自动轮播
+                      autoplay: (controller.homeList.value.length >1) ?true :false,
+                    ),
+                  ),
+                  Positioned(
+                    right: ScreenAdapter.width(0),
+                    top: ScreenAdapter.height(20),
+                    child: InkWell(
+                      onTap: (){
+                        Get.toNamed('/middlewaresettingpage', arguments: {"machineCode": controller.machineCode.value});
+                      },
+                      child: Container(
+                        height: ScreenAdapter.height(150),
+                        width: ScreenAdapter.width(200),
+                        alignment: Alignment.centerRight,
+                        padding: EdgeInsets.only(top:ScreenAdapter.height(20),left: ScreenAdapter.width(20),right: ScreenAdapter.width(20),bottom: ScreenAdapter.height(20)),
+                        child: Center(
+                          //加上Center让文字居中
+                          child: Text(
+                            "",
+                            style: TextStyle(
+                                fontSize: ScreenAdapter.fontSize(48.0),
+                                color: ColorsUtil.hexToColor("#F9F9F9"),
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: ScreenAdapter.height(1450),
+                    child: Container(
+                      width: ScreenAdapter.width(1080),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          if(controller.machineLanguages_JP.value == true)
+                            InkWell(
+                              onTap: () {
+                                //_clearCartList();
+                                Get.toNamed('/self-checkoutscanningcode',arguments: {
+                                  "checkLanguage": "JP"
+                                });
+
+
+                              },
+                              child: Container(
+                                width: ScreenAdapter.width(217),
+                                height: ScreenAdapter.height(90),
+                                margin: EdgeInsets.only(right: ScreenAdapter.width(35)),
+                                decoration: BoxDecoration(
+                                  //color: Color(0x11111111),
+                                  image: DecorationImage(
+                                    //alignment: Alignment.topCenter,
+                                      image: AssetImage(GImage.getImageString("imgpublic", "home_button")),
+                                      fit: BoxFit.fill),
+                                ),
+                                child: Center(
+                                  //加上Center让文字居中
+                                  child: Text(
+                                    '日本語',
+                                    style: TextStyle(
+                                        fontSize: ScreenAdapter.fontSize(36.0),
+                                        color: ColorsUtil.hexToColor("#F9F9F9"),
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          //SizedBox(width:ScreenAdapter.width(35)),
+                          if(controller.machineLanguages_CH.value == true)
+                            InkWell(
+                              onTap: () {
+
+                                Get.toNamed('/self-checkoutscanningcode',arguments: {
+                                  "checkLanguage": "CH"
+                                });
+                              },
+                              child: Container(
+                                width: ScreenAdapter.width(217),
+                                height: ScreenAdapter.height(90),
+                                margin: EdgeInsets.only(right: ScreenAdapter.width(35)),
+                                decoration: BoxDecoration(
+                                  //color: Color(0x11111111),
+                                  image: DecorationImage(
+                                    //alignment: Alignment.topCenter,
+                                      image: AssetImage(GImage.getImageString("imgpublic", "home_button")),
+                                      fit: BoxFit.fill),
+                                ),
+                                child: Center(
+                                  //加上Center让文字居中
+                                  child: Text(
+                                    '中文',
+                                    style: TextStyle(
+                                        fontSize: ScreenAdapter.fontSize(36.0),
+                                        color: ColorsUtil.hexToColor("#F9F9F9"),
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          //SizedBox(width:ScreenAdapter.width(35)),
+                          if(controller.machineLanguages_EN.value == true)
+                            InkWell(
+                              onTap: () {
+                                Get.toNamed('/self-checkoutscanningcode',arguments: {
+                                  "checkLanguage": "EN"
+                                });
+                              },
+                              child: Container(
+                                width: ScreenAdapter.width(217),
+                                height: ScreenAdapter.height(90),
+                                margin: EdgeInsets.only(right: ScreenAdapter.width(35)),
+                                decoration: BoxDecoration(
+                                  //color: Color(0x11111111),
+                                  image: DecorationImage(
+                                    //alignment: Alignment.topCenter,
+                                      image: AssetImage(GImage.getImageString("imgpublic", "home_button")),
+                                      fit: BoxFit.fill),
+                                ),
+                                child: Center(
+                                  //加上Center让文字居中
+                                  child: Text(
+                                    'English',
+                                    style: TextStyle(
+                                        fontSize: ScreenAdapter.fontSize(36.0),
+                                        color: ColorsUtil.hexToColor("#F9F9F9"),
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          //SizedBox(width:ScreenAdapter.width(35)),
+                          if(controller.machineLanguages_KO.value == true)
+                            InkWell(
+                              onTap: () {
+                                Get.toNamed('/self-checkoutscanningcode',arguments: {
+                                  "checkLanguage": "KO"
+                                });
+                              },
+                              child: Container(
+                                width: ScreenAdapter.width(217),
+                                height: ScreenAdapter.height(90),
+                                decoration: BoxDecoration(
+                                  //color: Color(0x11111111),
+                                  image: DecorationImage(
+                                    //alignment: Alignment.topCenter,
+                                      image: AssetImage(GImage.getImageString("imgpublic", "home_button")),
+                                      fit: BoxFit.fill),
+                                ),
+                                child: Center(
+                                  //加上Center让文字居中
+                                  child: Text(
+                                    '한국말',
+                                    style: TextStyle(
+                                        fontSize: ScreenAdapter.fontSize(36.0),
+                                        color: ColorsUtil.hexToColor("#F9F9F9"),
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  )
+
+                ],
+              ) // 没有数据时显示图片
                   : ListView(
                 shrinkWrap: true,
                 children: controller.ordersqlcontroller.cartItems.map((d) => generateCartList(context, d)).toList(),
