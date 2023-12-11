16e9e6b04fadc71ff12a74089b0d081c3896a15a
aaron.hou <aaron.hou@51fanxing.co.jp>
2023-12-11 11:24:14 +0900

    修复pay pay 等需要pos机退款的退款流程。

--- lib/app/modules/reimburseOrder/controllers/reimburse_order_controller.dart
+++ lib/app/modules/reimburseOrder/controllers/reimburse_order_controller.dart
@@ -205,7 +205,7 @@ LogUtil.d(response);
     request('webBootReimburseExecute', method: 'POST', parameters: formData)
         .then((value) {
       var response = json.decode(value.toString());
-      if(response['code'] == 200 &&  response['data']["executeMark"] == true){
+      if(response['code'] == 200 &&  response['data']["executeMark"] == true && response['data']["requestMessage"] ==""){
         EasyLoading.dismiss();
         _printReimburseReceipt(reimbursePrintViewSize, reimbursePrintView);//打印
         Get.dialog(
@@ -226,6 +226,9 @@ LogUtil.d(response);
       }else if(response['code'] == 200 && response['data']["executeMark"] == false && response['data']["requestMessage"] !=""){
         //showPosEasyLoading();
         payconnectSocker(questData: response['data']["requestMessage"]);
+      }else if(response['code'] == 200 && response['data']["executeMark"] == true && response['data']["requestMessage"] !=""){
+      //showPosEasyLoading();
+      payconnectSocker(questData: response['data']["requestMessage"]);
       }else{
         refundFailedAlert();
       }
