a1ee5a6167b17fea52b26642fc14dc04604d3134
aaron.hou <aaron.hou@51fanxing.co.jp>
2023-12-15 11:16:25 +0900

    修复只有现金支付时，没有选择打印页面。

--- lib/app/modules/menuPage/controllers/menu_page_controller.dart
+++ lib/app/modules/menuPage/controllers/menu_page_controller.dart
@@ -1167,19 +1167,19 @@ print("加1了");
           shopCartTotalPrice.value = response['data']["total"].toString();
 
           //只有现金，并且其余都为false的时候，直接跳转支付
-          if(showCash.value == true &&
-              isAllowPos.value == "0" &&
-              showAlipay.value == false &&
-              showWechat.value == false &&
-              showPayPay.value == false
-          ){
-            payment_method_num.value = "1";
-            //postNewOrderId();
-            gotoSettlement();
-          }else{
-            showSelectMealTypeAndPaymentMethodDialog();
-          }
-
+          // if(showCash.value == true &&
+          //     isAllowPos.value == "0" &&
+          //     showAlipay.value == false &&
+          //     showWechat.value == false &&
+          //     showPayPay.value == false
+          // ){
+          //   payment_method_num.value = "1";
+          //   //postNewOrderId();
+          //   gotoSettlement();
+          // }else{
+          //   showSelectMealTypeAndPaymentMethodDialog();
+          // }
+          showSelectMealTypeAndPaymentMethodDialog();
 
 
         }else{
