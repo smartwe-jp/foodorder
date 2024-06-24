fd430ea5ae3c5ad845d8a4940e0d5a7294ed17a0
aaron.hou <aaron.hou@51fanxing.co.jp>
2024-06-24 13:50:55 +0900

    modify tab color from server

--- lib/app/modules/menuPage/controllers/menu_page_controller.dart
+++ lib/app/modules/menuPage/controllers/menu_page_controller.dart
@@ -383,7 +383,7 @@ class MenuPageController extends GetxController with StateMixin {
             "categoryCode": categoryVoList['categoryCode'],
             "categoryName": categoryVoList['categoryName'],
             "showType": categoryVoList['showType'],
-            "showColor":MenuColor[menuIndex]
+            "showColor":categoryVoList['color'] ?? MenuColor[menuIndex]
           });
           menuIndex++;
           //配置顶部菜单默认项
