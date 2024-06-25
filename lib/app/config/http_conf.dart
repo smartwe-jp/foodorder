9726dfeb933028df7021b54ccc7c480171e3b5d0
aaron.hou <aaron.hou@51fanxing.co.jp>
2024-06-25 15:37:02 +0900

    update url

--- lib/app/config/http_conf.dart
+++ lib/app/config/http_conf.dart
@@ -1,6 +1,6 @@
 //const base_url = "https://waiter.smartwe.co.jp/";  //生产环境地址
-const base_url = "https://api.smartwe.jp/";  //new生产环境地址
-//const base_url = "https://sit-reji.smartwe.jp/";  //测试环境地址
+//const base_url = "https://api.smartwe.jp/";  //new生产环境地址
+const base_url = "https://sit-api.smartwe.jp/";  //测试环境地址
 
 const oa_base_url = "https://oa.gutingjun.com/api/";  //刷脸正式环境地址
 
