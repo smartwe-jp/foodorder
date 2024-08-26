3dab3969ca71899488f373655ab52514e5186f64
Aaronhou <AaronHou@51fanxing.co.jp>
2024-08-26 10:13:11 +0800

    add user storage clear function

--- lib/app/services/Storage.dart
+++ lib/app/services/Storage.dart
@@ -2,6 +2,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class Storage{
 
+  static Future<void> clearAll() async{
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    await sp.clear();
+  }
+
   static Future<void> setString(key,value) async{
        SharedPreferences sp=await SharedPreferences.getInstance();
        sp.setString(key, value);
