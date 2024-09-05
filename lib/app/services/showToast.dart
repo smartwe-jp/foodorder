ed4f98b49ae0e46ff7b7ae4ac455f0f8a412e6cd
Aaronhou <AaronHou@51fanxing.co.jp>
2024-09-05 15:13:48 +0900

    modify windows toast implement

--- lib/app/services/showToast.dart
+++ lib/app/services/showToast.dart
@@ -4,18 +4,47 @@ import 'dart:io';
 import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:foodorder/app/config/colorsUtil.dart';
+import 'package:foodorder/app/config/font.dart';
+import 'package:foodorder/app/services/ScreenAdapter.dart';
+import 'package:get/get.dart';
 //import 'package:win_toast/win_toast.dart';
+
 
 Future showToast(String msg, {context}) async {
   //const winMsg = const String.fromEnvironment('msg');
   if (Platform.isWindows) {
-      showOkAlertDialog(
-        context: context,
-        title: 'ご注意',
-        message: '$msg',
-        okLabel: 'はい',
+      if (Get.context != null) {
+        FToast fToast = FToast();
+        fToast.init(Get.context!);
+        fToast.showToast(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              color: const Color.fromARGB(255, 95, 98, 96),
+            ),
+            child: Text(
+              msg,
+            style: TextStyle(
+                    fontFamily: GFont.getFontFamily(),
+                    fontSize: ScreenAdapter.fontSize(18),
+                    fontWeight: FontWeight.w400,
+                    color: ColorsUtil.hexToColor("#FFFFFF"),
+                  ),
+            ),
+          ),
+          gravity: ToastGravity.CENTER,
+        );
+      }
+  
+      // showOkAlertDialog(
+      //   context: context,
+      //   title: 'ご注意',
+      //   message: '$msg',
+      //   okLabel: 'はい',
         
-      );
+      // );
     // showDialog<String>(
     //     context: context,
     //     builder: (BuildContext context) => Dialog(
