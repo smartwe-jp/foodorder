ab2698140fca626234aa32d118567c6084667177
Aaron <projectgame@163.com>
2025-04-28 14:24:58 +0900

    Fix when request receipt failure can not return back

--- lib/app/modules/settlement/controllers/settlement_controller.dart
+++ lib/app/modules/settlement/controllers/settlement_controller.dart
@@ -1116,6 +1116,7 @@ class SettlementController extends GetxController with StateMixin {
               doPrintOrderMenu(printType, times: times + 1);
             },
             cancle: () {
+              Get.back();
               commonCancel();
               FirebaseAnalytics.instance.logEvent(name: "settlement_order_error",parameters: {
                 "machineCode": machineInfo.machineCode,
