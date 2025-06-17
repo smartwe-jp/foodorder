36f39e0249b7a3ad2b2bd0ee234bb1138a94e2fe
Aaronhou <AaronHou@51fanxing.co.jp>
2025-06-17 13:46:59 +0900

    add api

--- lib/app/config/http_conf.dart
+++ lib/app/config/http_conf.dart
@@ -1,6 +1,6 @@
 //const base_url = "https://waiter.smartwe.co.jp/";  //生产环境地址
-const base_url = "https://api.smartwe.jp/";  //new生产环境地址
-//const base_url = "https://sit-api.smartwe.jp/"; //测试环境地址
+//const base_url = "https://api.smartwe.jp/";  //new生产环境地址
+const base_url = "https://sit-api.smartwe.jp/"; //测试环境地址
 
 const file_url = "https://app.smartwe.co.jp/";
 
@@ -123,6 +123,10 @@ const servicePath = {
   'webMachineNearFull': base_url + 'web/glory/full/notice',
 
   'webMachineFull': base_url + 'web/glory/stop/notice',
+
+  'webBootStartSelling': base_url + 'web/boot/start/selling',
+
+  'webBootStopSelling': base_url + 'web/boot/stop/selling',
 
   'localOrderPost': 'http://172.50.10.84:3000/api/add/order',
 };
