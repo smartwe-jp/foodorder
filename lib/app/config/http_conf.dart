468c38643629881b081dc2aeb6b09661307ae15d
Aaron <projectgame@163.com>
2025-06-18 13:13:16 +0900

    add api

--- lib/app/config/http_conf.dart
+++ lib/app/config/http_conf.dart
@@ -1,6 +1,6 @@
 //const base_url = "https://waiter.smartwe.co.jp/";  //生产环境地址
-const base_url = "https://api.smartwe.jp/";  //new生产环境地址
-//const base_url = "https://sit-api.smartwe.jp/";  //测试环境地址
+//const base_url = "https://api.smartwe.jp/";  //new生产环境地址
+const base_url = "https://sit-api.smartwe.jp/";  //测试环境地址
 
 const oa_base_url = "https://oa.gutingjun.com/api/";  //刷脸正式环境地址
 
@@ -85,5 +85,11 @@ const servicePath = {
   'webBootTroubleNotify': base_url + 'pad/web/boot/notice',
 
   'webBootCalculateConfirm': base_url + 'web/boot/calculate/confirm',
+
+  'webBootStartSelling': base_url + 'web/boot/start/selling',
+
+  'webBootStopSelling': base_url + 'web/boot/stop/selling',
+
+  'localOrderPost': 'http://172.50.10.84:3000/api/add/order',
 
 };
