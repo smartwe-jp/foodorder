cb9d068bbf3e6c8062d27343de4d9bb52b7c0d34
Aaron <projectgame@163.com>
2025-07-03 13:25:27 +0900

    remove await for print task

--- lib/app/modules/settlement/controllers/settlement_controller.dart
+++ lib/app/modules/settlement/controllers/settlement_controller.dart
@@ -1006,11 +1006,11 @@ class SettlementController extends GetxController with StateMixin {
           if(response['data']["orderType"] == 1 && is_allow_receipt_menu.value == "1"){
             //debugPrint("response['data']====${response['data']}");
             //_tpPrintnew(response['data'], printType);
-            await createPrintImageController.tpPrintnew(print_paper_txt_size, response['data'], printType);
+            createPrintImageController.tpPrintnew(print_paper_txt_size, response['data'], printType);
           }else{
             if (printType == "1") {
               //_tpPrintReceipt(response['data']);
-              await createPrintImageController.tpPrintReceipt(print_paper_txt_size, response['data']);
+              createPrintImageController.tpPrintReceipt(print_paper_txt_size, response['data']);
             }
           }
 
