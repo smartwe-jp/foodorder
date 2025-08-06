b8cb8fb4be986724bcfd82489bc45c5610bd5bf6
Aaron <projectgame@163.com>
2025-08-06 15:14:34 +0900

    modify menu api and print size

--- lib/app/config/http_conf.dart
+++ lib/app/config/http_conf.dart
@@ -11,9 +11,9 @@ const servicePath = {
   'webBootIndexv1': base_url + 'pad/web/boot/index/v1', //获取首页菜单地址
 
   //新改版获取分类菜单
-  'webBootIndexCategoryv2': base_url + 'pad/web/boot/index/category/v2', //获取首页分类
+  'webBootIndexCategoryv2': base_url + 'pad/web/boot/index/category/v0', //获取首页分类
   'webBootIndexMenuv2': base_url + 'pad/web/boot/index/menu/v2', //获取某分类菜单
-  'webBootIndexMenuv3': base_url + 'pad/web/boot/index/menu/v3', //获取某分类菜单
+  'webBootIndexMenuv3': base_url + 'pad/web/boot/index/menu/v0', //获取某分类菜单
 
   'webStockBooking': base_url + 'pad/web/boot/stock-booking', //限量请求地址
   //'webBootOrder': base_url + 'pad/web/boot/order', //提交订单请求地址
