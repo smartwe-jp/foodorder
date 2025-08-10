704a9e9b2b502f6b1f9efdaa5a718a165d18d686
Aaronhou <AaronHou@51fanxing.co.jp>
2025-08-10 11:15:58 +0900

    modify API

--- lib/app/config/http_conf.dart
+++ lib/app/config/http_conf.dart
@@ -18,7 +18,7 @@ const servicePath = {
   'webBootIndexCategoryEdit': base_url + 'pad/web/boot/index/category/v0', //获取首页分类
   'webBootIndexMenuv2': base_url + 'pad/web/boot/index/menu/v2', //获取某分类菜单
   'webBootIndexMenuv3': base_url + 'pad/web/boot/index/menu/v3', //获取某分类菜单
-  'webBootIndexMenuvEdit': base_url + 'pad/web/boot/index/menu/v0', //获取某分类菜单
+  'webBootIndexMenuEdit': base_url + 'pad/web/boot/index/menu/v0', //获取某分类菜单
 
   'webStockBooking': base_url + 'pad/web/boot/stock-booking', //限量请求地址
   //'webBootOrder': base_url + 'pad/web/boot/order', //提交订单请求地址
