e519536763c7d1fdadba73699b325ccf08887c4d
Aaron <projectgame@163.com>
2025-09-09 09:17:57 +0900

    Fix get size error

--- lib/app/modules/settlement/controllers/settlement_controller_printer_extension.dart
+++ lib/app/modules/settlement/controllers/settlement_controller_printer_extension.dart
@@ -181,8 +181,8 @@ class PrintService extends GetxService {
         // 先打印票号和基本信息
         final Queue<Widget> labelPrintQueue = Queue<Widget>();
         final printSize = printer['labelSize'] ?? '300x225';
-        final printWidth = printSize.split('x')[0].trim().toInt(); // 获取标签宽度
-        final printHeight = printSize.split('x')[1].trim().toInt(); // 获取标签高度
+        final printWidth = int.tryParse(printSize.split('x')[0]) ?? 300; // 获取标签宽度
+        final printHeight = int.tryParse(printSize.split('x')[1]) ?? 225; // 获取标签高度
         // Add the head receipt widget to the print queue
         var totalQty = 0;
         for (var item in items) {
