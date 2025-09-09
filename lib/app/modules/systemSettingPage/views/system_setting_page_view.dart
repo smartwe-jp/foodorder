d68df8651cae4269622d8ad873bd2789a34bcf02
Aaronhou <AaronHou@51fanxing.co.jp>
2025-09-09 18:03:02 +0900

    remove setting logic

--- lib/app/modules/systemSettingPage/views/system_setting_page_view.dart
+++ lib/app/modules/systemSettingPage/views/system_setting_page_view.dart
@@ -2793,7 +2793,7 @@ class SystemSettingPageView extends GetView {
                               },
                                 defaultVerticalAlignment: TableCellVerticalAlignment.middle,
                                 children: <TableRow>[
-                                  if(controller.actuarial.value == true)
+                                  //if(controller.actuarial.value == true)
                                   TableRow(
                                       children: <Widget>[
                                         Container(
