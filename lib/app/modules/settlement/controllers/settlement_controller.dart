d166458afcf84219ceab7ffd4b9032996e2be57b
Aaronhou <AaronHou@51fanxing.co.jp>
2025-09-29 13:20:40 +0900

    modify return flow

--- lib/app/modules/settlement/controllers/settlement_controller.dart
+++ lib/app/modules/settlement/controllers/settlement_controller.dart
@@ -456,7 +456,7 @@ class SettlementController extends GetxController with StateMixin {
       case MachineMode.sell:
       case MachineMode.takeout:
         if (machineInfo.isBackHome) {
-          await Get.offNamedUntil(Routes.MENU_PAGE, (route) => route.settings.name == Routes.TRANSIT_PAGE);
+          await Get.offNamedUntil(Routes.MENU_PAGE, (route) => route.settings.name == Routes.CHECKOUT_PAGE);
         } else {
           await Get.offNamedUntil(Routes.CHECKOUT_PAGE, (route) => route.settings.name == Routes.TRANSIT_PAGE);
         }
--- lib/app/modules/settlement/result/logic.dart
+++ lib/app/modules/settlement/result/logic.dart
@@ -68,7 +68,7 @@ class ResultLogic extends GetxController {
       case MachineMode.sell:
       case MachineMode.takeout:
         if (machineInfo.isBackHome) {
-          await Get.offNamedUntil(Routes.MENU_PAGE, (route) => route.settings.name == Routes.TRANSIT_PAGE);
+          await Get.offNamedUntil(Routes.MENU_PAGE, (route) => route.settings.name == Routes.CHECKOUT_PAGE);
         } else {
           await Get.offNamedUntil(Routes.CHECKOUT_PAGE, (route) => route.settings.name == Routes.TRANSIT_PAGE);
         }
