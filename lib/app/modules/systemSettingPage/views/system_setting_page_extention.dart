92991f5232a0fa480a700e0dfc7faad2e058fc60
Aaron <projectgame@163.com>
2025-10-07 11:45:42 +0900

    modify sse ui

--- lib/app/modules/systemSettingPage/views/system_setting_page_extention.dart
+++ lib/app/modules/systemSettingPage/views/system_setting_page_extention.dart
@@ -90,12 +90,11 @@ extension SystemSettingPageExtension on SystemSettingPageView {
             bottom: ScreenAdapter.height(3), right: ScreenAdapter.width(20)),
         child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
             spacing: ScreenAdapter.height(10),
             children: [
               if (needInput)
-              Container(
-                margin: EdgeInsets.only(left: ScreenAdapter.width(20)),
-                child:
+              
                 InkWell(
                   highlightColor: Colors.transparent, // 透明色
                   splashColor: Colors.transparent,
@@ -103,7 +102,7 @@ extension SystemSettingPageExtension on SystemSettingPageView {
                     controller.editSSESetting(name, address, identify, isOn);
                   },
                   child: Container(
-                    margin: EdgeInsets.only(left: ScreenAdapter.width(20)),
+                    //margin: EdgeInsets.only(left: ScreenAdapter.width(20)),
                     //设置 child 居中
                     alignment: Alignment(0, 0),
                     height: ScreenAdapter.height(60),
@@ -117,14 +116,7 @@ extension SystemSettingPageExtension on SystemSettingPageView {
                       //设置四周边框
                       //border: new Border.all(width: 1, color: Colors.red),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
+                    child: Text(
                             identify.isEmpty ? "ターチして設置":identify,
                                 style: TextStyle(
                                   fontFamily: 'NotoSansJP',
@@ -132,13 +124,9 @@ extension SystemSettingPageExtension on SystemSettingPageView {
                                   color: ColorsUtil.hexToColor("#FFFFFF"),
                                 )
                             ),
-                          ],
-                        )
-                      ],
-                    ),
                   ),
                 ),
-              ),
+            
 
               //switchButton ios type
               if (identify.isNotEmpty)
