f5e779f0d4ebcf753abe918ca14c3eb3f3deb3cc
Aaron <projectgame@163.com>
2025-10-07 14:34:34 +0900

    add gap text

--- lib/app/modules/settlement/controllers/settlement_controller_printer_extension.dart
+++ lib/app/modules/settlement/controllers/settlement_controller_printer_extension.dart
@@ -183,7 +183,7 @@ class PrintService extends GetxService {
         final printHeight = int.tryParse(printSize.split('x')[1]) ?? 225; // 获取标签高度
         // Add the head receipt widget to the print queue
         debugPrint("Label Print Width: $printWidth, Height: $printHeight");
-        final time = orderId + " " + await DateTime.now().toString().substring(5, 16);
+        final time = orderId + "#" + await DateTime.now().toString().substring(5, 16);
         var totalQty = 0;
         for (var item in items) {
           totalQty += (item["qty"] ?? 0) as int;
