f638c556caae6539043f8f1966c67cb48cb6c720
Aaron <projectgame@163.com>
2025-10-22 14:54:14 +0900

    Fix mode error

--- lib/app/controllers/machine_info.dart
+++ lib/app/controllers/machine_info.dart
@@ -131,7 +131,7 @@ class MachineInfoController extends GetxController {
   }
 
   bool get isTakeoutMode {
-    return currentMode == MachineMode.checkout || currentMode == MachineMode.scan;
+    return currentMode == MachineMode.takeout || currentMode == MachineMode.scan;
   }
 
   String get printType {
