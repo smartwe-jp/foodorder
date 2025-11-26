44d7bad33c6466cb0064e6ad32564f4ceb827330
Aaronhou <AaronHou@51fanxing.co.jp>
2025-11-26 13:28:06 +0900

    modify return setting

--- lib/app/modules/settlement/result/logic.dart
+++ lib/app/modules/settlement/result/logic.dart
@@ -94,9 +94,9 @@ class ResultLogic extends GetxController {
       case MachineMode.sell:
       case MachineMode.takeout:
         if (machineInfo.isBackHome) {
-          await Get.offNamedUntil(Routes.MENU_PAGE, (route) => route.settings.name == Routes.CHECKOUT_PAGE);
-        } else {
           await Get.offNamedUntil(Routes.CHECKOUT_PAGE, (route) => route.settings.name == Routes.TRANSIT_PAGE);
+        } else {
+          await Get.offNamedUntil(Routes.MENU_PAGE, (route) => route.settings.name == Routes.CHECKOUT_PAGE);
         }
         break;
       case MachineMode.scan:
