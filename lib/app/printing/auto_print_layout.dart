78ac96e161855d1bf37c321e8cbdb6d2492865e0
Aaron <projectgame@163.com>
2026-01-28 14:05:34 +0900

    modify print font size

--- lib/app/printing/auto_print_layout.dart
+++ lib/app/printing/auto_print_layout.dart
@@ -200,7 +200,7 @@ class PrintTextStyles {
     fontFamily: 'NotoSansJP',
     color: Colors.black,
     fontSize: 24,
-    fontWeight: FontWeight.w400,
+    fontWeight: FontWeight.w300,
   );
 
   static const TextStyle menBold = TextStyle(
