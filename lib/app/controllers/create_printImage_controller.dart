c5302682c66838ceb110e22e1a4de5ba9688ea12
Aaronhou <AaronHou@51fanxing.co.jp>
2026-01-28 09:28:49 +0900

    modify print gap time

--- lib/app/controllers/create_printImage_controller.dart
+++ lib/app/controllers/create_printImage_controller.dart
@@ -58,7 +58,7 @@ class CreatePrintImageController extends GetxController {
 
     _sendToUsePrinter(kitchenTicketWidget);
     if (printType == "1") {
-      await Future.delayed(Duration(milliseconds: 500));
+      await Future.delayed(Duration(milliseconds: 800));
 
       Widget receiptWidget = buildReceiptWidget(printData, width: printWidth, printOptions: printReceiptOptions);
       _sendToUsePrinter(receiptWidget);
