3ddb6c85fccdb3f9ff3fee188f20749246fd75c0
Aaronhou <AaronHou@51fanxing.co.jp>
2026-06-04 16:46:46 +0900

    modify launch flow

--- lib/app/modules/TransitPage/controllers/transit_page_controller.dart
+++ lib/app/modules/TransitPage/controllers/transit_page_controller.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 //import 'package:foodorder/app/controllers/machine_info_controller.dart';
 import 'package:foodorder/app/plugins/appset/lib/appset.dart';
+import 'package:foodorder/app/services/CustomLogerHandler.dart';
 import 'package:get/get.dart';
 import 'package:logging/logging.dart';
 import 'package:foodorder/app/config/http_conf.dart';
@@ -123,16 +124,17 @@ class TransitPageController extends GetxController {
         _getSmartweSystemSettingInfo();
         return;
       }
-      logger.info('-- getMachineActivate with loadActive --');
+      
       final formData = {
         "machineCode": _machineCode.value,
         "version": local_version.value
       };
+      logI('-- getMachineActivate with loadActive -- machineCode: ${_machineCode.value}, version: ${local_version.value} --');
       final val = await request('webBootActivatev3', method: 'POST', parameters: formData)
         .timeout(const Duration(seconds: 10));
       final response = json.decode(val.toString());
 
-      logger.info("getMachineActivate response: $response");
+      logI("getMachineActivate response: $response");
       
         if (response != null &&
             response['code'] == 200 &&
@@ -288,7 +290,7 @@ class TransitPageController extends GetxController {
         _showErrorDialog(isActive: response['data'] == null);
       
       } catch(e) {
-        logger.warning('getMachineActivate error: $e');
+        logW('getMachineActivate error: $e');
         if (Platform.isAndroid) {
           // FirebaseAnalytics.instance.logEvent(
           //     name: 'machine_activate_error',
@@ -304,7 +306,7 @@ class TransitPageController extends GetxController {
           _showErrorDialog(error: e);
         }
       } finally {
-        logger.info('activation finished');
+        logI('activation finished');
         //_activating = false;
       }
   }
@@ -359,83 +361,25 @@ class TransitPageController extends GetxController {
   }
 
   _getSmartweSystemSettingInfo() async {
-    logger.info("getSmartweSystemSettingInfo");
-    Map systemSettingInfo = await HomeServices.getSystemSettingInfo();
+    logI("getSmartweSystemSettingInfo");
+    try {
+      Map systemSettingInfo = await HomeServices.getSystemSettingInfo();
 
-    // var checkmachineMode = "1";
-    // if (SystemSettingInfo["machineMode"] != "" &&
-    //     SystemSettingInfo["machineMode"] != null &&
-    //     SystemSettingInfo["machineMode"] != "1") {
-    //   if (_actuarial.value == true) {
-    //     checkmachineMode =
-    //         (SystemSettingInfo["machineMode"] == "3") ? "3" : "2";
-    //   } else {
-    //     checkmachineMode = "1";
-    //   }
-    // }
-    // var systemSettingData = {
-    //   "diningType": (SystemSettingInfo["diningType"] != "" &&
-    //           SystemSettingInfo["diningType"] != null)
-    //       ? SystemSettingInfo["diningType"]
-    //       : "1", //1堂食 2外带
-    //   "menuDirection": (SystemSettingInfo["menuDirection"] != "" &&
-    //           SystemSettingInfo["menuDirection"] != null)
-    //       ? SystemSettingInfo["menuDirection"]
-    //       : "1", //1顶部横向 2左侧竖
-    //   //"printPaperSize":(SystemSettingInfo["printPaperSize"] !="" && SystemSettingInfo["printPaperSize"]!=null) ? SystemSettingInfo["printPaperSize"] :"1",//1 58mm 2 80mm
-    //   "printPaperTxtSize":(SystemSettingInfo["printPaperTxtSize"] !="" && SystemSettingInfo["printPaperTxtSize"]!=null) ? SystemSettingInfo["printPaperTxtSize"] :"1",//1 普通　2大　3特大
-    //   "isAllowReceipt":(SystemSettingInfo["isAllowReceipt"] !="" && SystemSettingInfo["isAllowReceipt"]!=null) ? SystemSettingInfo["isAllowReceipt"] :"1",//1必须打印小票 2不必须
-    //   "isAllowReceiptMenu":(SystemSettingInfo["isAllowReceiptMenu"] !="" && SystemSettingInfo["isAllowReceiptMenu"]!=null) ? SystemSettingInfo["isAllowReceiptMenu"] :"1",//1必须打印小票顶部菜单 2不打印
-    //   //"machineMode":checkmachineMode,
-    //   "isReservation":(SystemSettingInfo["isReservation"] !="" && SystemSettingInfo["isReservation"]!=null) ? SystemSettingInfo["isReservation"] :"0",//是否开启预约 0不开启 1开启
-    //   "isAllowAttendance":(SystemSettingInfo["isAllowAttendance"] !="" && SystemSettingInfo["isAllowAttendance"]!=null) ? SystemSettingInfo["isAllowAttendance"] :"0",//0 不开考勤 1开考勤
-    //   "isAllowOneYen":(SystemSettingInfo["isAllowOneYen"] !="" && SystemSettingInfo["isAllowOneYen"]!=null) ? SystemSettingInfo["isAllowOneYen"] :"0",//0禁用1元 1不禁用
-    //   "isAllow5000": SystemSettingInfo["isAllow5000"] ?? "1",
-    //   "isAllow10000": SystemSettingInfo["isAllow10000"] ?? "1",
-    //   "isAllowBackHome":(SystemSettingInfo["isAllowBackHome"] !="" && SystemSettingInfo["isAllowBackHome"]!=null) ? SystemSettingInfo["isAllowBackHome"] :"0",//0回到首页，1回到菜单页
-    //   "isAllowPos":(SystemSettingInfo["isAllowPos"] !="" && SystemSettingInfo["isAllowPos"]!=null) ? SystemSettingInfo["isAllowPos"] :"0",//0 不开pos 1开pos
-    //   "isAllowWlanPrint":(SystemSettingInfo["isAllowWlanPrint"] !="" && SystemSettingInfo["isAllowWlanPrint"]!=null) ? SystemSettingInfo["isAllowWlanPrint"] :"0",//0 不开打印机 1开打印机
-    //   "isAllowWlanPrintContinuous":(SystemSettingInfo["isAllowWlanPrintContinuous"] !="" && SystemSettingInfo["isAllowWlanPrintContinuous"]!=null) ? SystemSettingInfo["isAllowWlanPrintContinuous"] :"1",//0 单票 1连票
-    //   "showPrintType":(SystemSettingInfo["showPrintType"] !="" && SystemSettingInfo["showPrintType"]!=null) ? SystemSettingInfo["showPrintType"] :"0",//0 receipt 1label
-    //   "isAllowWlanPrintTwo":(SystemSettingInfo["isAllowWlanPrintTwo"] !="" && SystemSettingInfo["isAllowWlanPrintTwo"]!=null) ? SystemSettingInfo["isAllowWlanPrintTwo"] :"0",//0 不开打印机 1开打印机
-    //   "isAllowWlanPrintTwoContinuous":(SystemSettingInfo["isAllowWlanPrintTwoContinuous"] !="" && SystemSettingInfo["isAllowWlanPrintTwoContinuous"]!=null) ? SystemSettingInfo["isAllowWlanPrintTwoContinuous"] :"1",//0 单票 1连票
-    //   "isAllowRejishime":(SystemSettingInfo["isAllowRejishime"] !="" && SystemSettingInfo["isAllowRejishime"]!=null) ? SystemSettingInfo["isAllowRejishime"] :"0",
-    //   "panelType": SystemSettingInfo['panelType'] ?? 'Mini',
-    //   "isAllowWlanPanelPrint": SystemSettingInfo["isAllowWlanPanelPrint"] ?? '0'
-    // };
-    // Storage.setString('smartwe_systemSetting',
-    //     json.encode(systemSettingData)); //1 默认58mm  2 宽纸80mm
-    // GetxStorage.setData(
-    //     'smartwe_systemSetting', json.encode(systemSettingData));
-    //}
+    final smartweMachineSettingPassword = await HomeServices.getMachineSettingManagePasswordInfo();
 
-
-    var smartweMachineSettingPassword = await HomeServices.getMachineSettingManagePasswordInfo();
     if(smartweMachineSettingPassword != null && smartweMachineSettingPassword!= ""){
       Storage.setString('machineSettingManagePassword', smartweMachineSettingPassword);
       GetxStorage.setData('machineSettingManagePassword', smartweMachineSettingPassword);
 
     }
-
-    
-
-    //这里判断是否禁用1元
-
-    //_goNext(checkmachineMode);
-
-    //_goNext(checkmachineMode);
-     _injectControllers(systemSettingInfo);
+     await _injectControllers(systemSettingInfo);
+    } catch (e) {
+      logW("getSmartweSystemSettingInfo error: $e");
+      
+    }
   }
 
   Future _injectControllers(systemSettingData) async {
-    // if (Get.isRegistered<PosPayController>()) Get.delete<PosPayController>();
-    // Get.put(PosPayController());
-
-    //  if (Get.isRegistered<MachineInfoController>()){
-    //   Get.delete<MachineInfoController>();
-    //  } 
-
-    // Get.put(MachineInfoController(systemSettingData));
 
     if (!Get.isRegistered<MachineInfoController>()) {
       final controller = MachineInfoController(systemSettingData);
@@ -448,8 +392,6 @@ class TransitPageController extends GetxController {
       await Get.find<MachineInfoController>().updateMachineSettingInfo(settingInfo: systemSettingData);
       debugPrint('update MachineInfoController done');
     }
-    // await Get.delete<MachineInfoController>();
-    // Get.put(MachineInfoController(systemSettingData));
   
 
     MachineInfoController machineInfo = Get.find<MachineInfoController>();
@@ -506,13 +448,6 @@ class TransitPageController extends GetxController {
   Future goNext() async {
     Get.updateLocale(Locale('jp', 'JP'));
     _goCheckOut();
-    // if(checkmachineMode == "2"){
-    //  _goCheckOut();
-    // }else if(checkmachineMode == "3"){
-    //   _goSelfService();
-    // }else{
-    //   _goMain();
-    // }
   }
 
   void _goMain() async {
