a5a9b866e34b1961cb3eafc14df647b781dafbd0
lky <liukeyan915@163.com>
2026-06-24 09:46:07 +0900

    适配flutter 3.44

---
+++ macos/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -0,0 +1,46 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+// Generated file. Do not edit.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "FlutterGeneratedPluginSwiftPackage",
+    platforms: [
+        .macOS("10.15")
+    ],
+    products: [
+        .library(name: "FlutterGeneratedPluginSwiftPackage", type: .static, targets: ["FlutterGeneratedPluginSwiftPackage"])
+    ],
+    dependencies: [
+        .package(name: "connectivity_plus", path: "../.packages/connectivity_plus-6.1.5"),
+        .package(name: "firebase_analytics", path: "../.packages/firebase_analytics-11.6.0"),
+        .package(name: "firebase_core", path: "../.packages/firebase_core-3.15.2"),
+        .package(name: "firebase_crashlytics", path: "../.packages/firebase_crashlytics-4.3.10"),
+        .package(name: "open_file_mac", path: "../.packages/open_file_mac-1.0.4"),
+        .package(name: "package_info_plus", path: "../.packages/package_info_plus-8.3.0"),
+        .package(name: "path_provider_foundation", path: "../.packages/path_provider_foundation-2.4.2"),
+        .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.4"),
+        .package(name: "sqflite_darwin", path: "../.packages/sqflite_darwin-2.4.2"),
+        .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
+    ],
+    targets: [
+        .target(
+            name: "FlutterGeneratedPluginSwiftPackage",
+            dependencies: [
+                .product(name: "connectivity-plus", package: "connectivity_plus"),
+                .product(name: "firebase-analytics", package: "firebase_analytics"),
+                .product(name: "firebase-core", package: "firebase_core"),
+                .product(name: "firebase-crashlytics", package: "firebase_crashlytics"),
+                .product(name: "open-file-mac", package: "open_file_mac"),
+                .product(name: "package-info-plus", package: "package_info_plus"),
+                .product(name: "path-provider-foundation", package: "path_provider_foundation"),
+                .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
+                .product(name: "sqflite-darwin", package: "sqflite_darwin"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ]
+        )
+    ]
+)
