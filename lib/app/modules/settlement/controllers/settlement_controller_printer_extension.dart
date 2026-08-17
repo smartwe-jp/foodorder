842c470470398e229fcc80f75dd1bd4d0cfcadb9
Aaron <projectgame@163.com>
2026-08-17 13:44:27 +0900

    modify kitchen ticket qty

--- lib/app/modules/settlement/controllers/settlement_controller_printer_extension.dart
+++ lib/app/modules/settlement/controllers/settlement_controller_printer_extension.dart
@@ -1399,7 +1399,7 @@ class PrintService extends GetxService {
       bool needOption = true,
       bool isContinuous = false,
       String? categoryName}) {
-    final optionQtyString = qty == 1 ? "" : "x $qty";
+    final itemQtyString = "x $qty";
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
       child: Column(
@@ -1431,7 +1431,7 @@ class PrintService extends GetxService {
                 ),
               ),
               Text(
-                optionQtyString,
+                itemQtyString,
                 style: TextStyle(
                   fontSize: 40,
                   color: ColorsUtil.hexToColor("#000000"),
